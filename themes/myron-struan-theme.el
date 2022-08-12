638759a191d1c27254375121e93fb2cf8950e363
neeasade <neeasade@gmail.com>
2022-08-12 17:40:55 -0500

    rebrand

--- themes/myron-struan-theme.el
+++ themes/myron-struan-theme.el
@@ -1,27 +1,24 @@
 ;; -*- lexical-binding: t; -*-
 ;; namesake: the Struan family, from James Clavell's Asian Saga series
 
-(require 'tarps)
+(require 'myron)
 
-(defun tarp/struan-get-colors (background)
+(defun myron-struan-colors (background)
   "get the foreground colors against a specific background"
-  (let*
-    ((colors (--> (ct-make-hsluv 265 60 40)
-               (ct-rotation-hsluv it 60)
-               (-map (fn (tarp/nth <> it))
-                 '(1 -1 2 3))
-               (-map (fn (ct-edit-hsluv-l <> 80)) it)
-               (-map (fn (ct-tint-ratio <> background 4.3)) it)))
+  (let* ((colors (->> (ct-rotation-hsluv (ct-make-hsluv 265 60 40) 60)
+                   (-select-by-indices '(1 5 2 3))
+                   (-map (fn (ct-edit-hsluv-l <> 80)))
+                   (-map (fn (ct-tint-ratio <> background 4.3)))))
 
-      (foreground (ct-tint-ratio background background 7))
+          (foreground (ct-tint-ratio background background 7))
 
-      (faded
-        (ct-tint-ratio
-          (ct-edit-hsl
-            (ct-tint-ratio background background 5.5)
-            ;; (nth 2 colors)
-            (lambda (h s l) (list h 80 70)))
-          background 4.3)))
+          (faded
+            (ct-tint-ratio
+              (ct-edit-hsl
+                (ct-tint-ratio background background 5.5)
+                ;; (nth 2 colors)
+                (lambda (h s l) (list h 80 70)))
+              background 4.3)))
 
     (ht
       (:background background)
@@ -39,7 +36,7 @@
 
 (-let*
   ((background (ct-make-lab 93 2 4))
-    (normal-parts (tarp/struan-get-colors background))
+    (normal-parts (myron-struan-colors background))
     ((&hash :alt :assumed :primary :faded :foreground) normal-parts)
 
     (background>
@@ -60,7 +57,7 @@
         (ct-edit-hsluv-l
           (ct-get-hsluv-l background>>)))))
 
-  (setq tarp/theme*
+  (setq myron-theme*
     (ht
       ;; focused/selected emphasis
       (:focused
@@ -72,12 +69,14 @@
       (:normal normal-parts)
 
       ;; weak emphasis
-      (:weak (tarp/struan-get-colors background>))
+      (:weak (myron-struan-colors background>))
 
       ;; strong emphasis
-      (:strong (tarp/struan-get-colors background>>)))))
+      (:strong (myron-struan-colors background>>)))))
 
-(deftheme tarp-struan)
-(tarp/base16-theme-define 'tarp-struan)
+(deftheme myron-struan)
+(myron-theme-define 'myron-struan)
 
-(provide-theme 'tarp-struan)
+(provide-theme 'myron-struan)
+(provide 'myron-struan-theme)
+;;; myron-struan-theme.el ends here
