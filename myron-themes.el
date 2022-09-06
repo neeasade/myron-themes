06d354816c8412ca3439dc0ea635202456f15335
neeasade <neeasade@users.noreply.github.com>
2022-09-05 21:51:47 -0500

    move package info

--- myron-themes.el
+++ myron-themes.el
@@ -1,5 +1,14 @@
 ;;; myron-themes.el --- color themes for emacs -*- coding: utf-8; lexical-binding: t -*-
+;; Copyright (C) 2022 neeasade
+;; SPDX-License-Identifier: MIT
+;; Author: neeasade <neeasade@gmail.com>
+;; URL: https://github.com/neeasade/myron-themes
+;; Package-Requires: ((emacs "26.1") (ct "0.2") (fn "0.1.2") (helpful "0.19") (s "1.12.0") (ht "2.3") (base16-theme "3.0"))
+;; Version: 0.1
+
 ;;; Commentary:
+;; A set of color themes built on top of the base16-themes.
+
 ;;; Code:
 (require 'myron)
 (provide 'myron-themes)
