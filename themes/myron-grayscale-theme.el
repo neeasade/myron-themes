2e5ceb879e83fb462ea96e263614dadbac71c5de
neeasade <neeasade@users.noreply.github.com>
2023-04-27 16:00:32 -0500

    myron-grayscale

---
+++ themes/myron-grayscale-theme.el
@@ -0,0 +1,39 @@
+;; -*- lexical-binding: t; -*-
+
+(require 'myron-themes)
+
+(defun myron-grayscale-colors (background)
+  "get the foreground colors against a specific background"
+  (->> '(:background 0
+          :foreground 6
+          :faded 3
+          :primary 4
+          :assumed 5
+          :alt 3
+          :strings 4)
+    (-partition 2)
+    (-mapcat (-lambda ((label contrast))
+               (list label (ct-contrast-min background background contrast))))
+    (ht<-plist)))
+
+(defun myron-grayscale-create ()
+  (-let* ((background "#f7f7f7"))
+    (->> '(:focused 3
+            :weak 4
+            :strong 7)
+      (-partition 2)
+      (-map (-lambda ((label distance))
+              (list label
+                (ct-iterate background 'ct-edit-lab-l-dec
+                  (fn (> (ct-distance <> background) distance))))))
+      (append `((:normal ,background)))
+      (-mapcat (-lambda ((label bg)) (list label (myron-grayscale-colors bg))))
+      (ht<-plist))))
+
+(deftheme myron-grayscale)
+(myron-theme-define 'myron-grayscale 'myron-grayscale-create (myron-grayscale-create))
+(myron-evil-cursor-color (myron-get :alt))
+
+(provide-theme 'myron-grayscale)
+(provide 'myron-grayscale-theme)
+;;; myron-grayscale-theme.el ends here
