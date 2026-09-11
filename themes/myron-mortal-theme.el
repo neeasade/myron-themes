8f980746bceb15b7d2786f52d0f2ed85e293aacf
neeasade <neeasade@users.noreply.github.com>
2026-09-11 08:48:48 -0500

    further smithing

--- themes/myron-mortal-theme.el
+++ themes/myron-mortal-theme.el
@@ -2,71 +2,72 @@
 ;; It's a shadowy dusk here, and I am merely mortal.
 ;; a theme wherein we play with the hct colorspace
 
-;; playing with hct, advice I read:
-;; use single hue, use tone for emphasis, and /maybe/ some chroma
-;; I shall complement
-
 (require 'myron-themes)
 
-(defun myron-mortal-colors (background)
+(defun myron-mortal-colors (background &optional print?)
   "Get the mortal foreground colors against a specific BACKGROUND."
-  ;; (let* (
-  ;;         ()
-  ;;         )
-  ;;   (ht<-plist
-  ;;     `(:background ,background
-  ;;        ;; thought; complement colors for fg?
-  ;;        :foreground ,(ct-contrast-min background background 5.0 'hct-t)
-  ;;        :faded ,(ct-contrast-min background background 3.0 'hct-t)
-  ;;        :assumed ,(ct-contrast-min background background 3.0 'hct-t)
-  ;;        :primary
-  ;;        :alt
-  ;;        :strings
-  ;;        )))
-  ;;
   (-let* (
-
            (hue (ct-get-hct-h background))
 
            ;; c is complement, a is original
            ;; ｔｅｔｒａｄｉｃ
-           ((a b c d) (-iota 4 hue 90))
+           ;; ((a b c d) (-iota 4 hue 90))
 
            ;; to consider:
-           ;; (o 30)
-           ;; ;; pairs: ac and bd
-           ;; ((a b c d)
-           ;;   (list
-           ;;     hue
-           ;;     (+ hue o)
-           ;;     (+ hue 180)
-           ;;     (+ hue o 180)))
+           (o 60)
+           ;; pairs: ac and bd
+           ((a b c d)
+             (list
+               hue
+               (+ hue o)
+               (+ hue 180)
+               (+ hue o 180)))
+           (contrast-boost .6))
 
-           (contrast-boost 0)
-           )
-    ;; (prn a b c d)
+    (when print?
+      ;; visualize
+      (->> (list a b c d)
+        (-map (lambda (hue)
+                (prn
+                  (-> background
+                    (ct-edit-hct-t-dec 10)
+                    (ct-edit-hct-h hue)
+                    (ct-edit-hct-c 50)))))))
+
+
     (->> (list
-           ;; contrast, chroma, hue
-           :foreground  5   6    a
-           :faded       3   60   a
-           :assumed     4   40   c
-           :strings     4   80   b
-           :alt         3   70  d
-           :primary     5   100  c
-           )
+           ;; contrast chroma hue
+           :assumed     4    40   a
+           :foreground  5    6    c
+           :primary     4.5  100  c
 
+           ;; todo: there's a problem here.
+           ;; we complect "highlighting" and "types"
+           ;; maybe "types" can be "highlighting" with less chroma
+           ;; we need a meta -> highlight color for character matching, can't get away with it here
+           :alt         3    45  b
+
+           ;; highlight value
+           ;; :alt         3    80  b
+
+           :faded       2.5    14   d
+           :strings     3    100   d
+           )
       (-partition 4)
-      (-mapcat (-lambda ((label contrast c-set hue))
+      (-mapcat (-lambda ((label contrast chroma hue))
                  (list label
                    ;; intuition: darken a smidge, fuck with it, then contrast it
                    (-> background
                      (ct-edit-hct-t-dec 10)
                      (ct-edit-hct (lambda (h c tt)
-                                    (list (or hue h)
+                                    (list hue
                                       ;; (* c-mult c)
-                                      c-set
+                                      chroma
+                                      ;; c
                                       tt)))
-                     (ct-contrast-min background (+ contrast-boost contrast) 'hct-t)))))
+                     ;; (myron-mortal-min background (+ contrast-boost contrast))
+                     (ct-contrast-min background (+ contrast-boost contrast) 'hct-t)
+                     ))))
       (-concat (list :background background))
       (ht<-plist))))
 
@@ -78,41 +79,48 @@
            (b seed)
 
            (b (ct-edit-hct-t-inc seed 2))
+
            (b (ct-edit-hct-t-inc seed 1.5))
 
-           ;; todo: bump chroma?
-           ;; thought: what if we used contrast to dec tone instead of a solid value
-           (b> (ct-edit-hct-t-dec seed 6))
-           (b>> (ct-edit-hct-t-dec seed 12))
+           ;; (b> (ct-edit-hct-t-dec seed 6))
+           ;; (b>> (ct-edit-hct-t-dec seed 12))
 
-           (b> (ct-edit-hct-t-dec seed 5))
-           (b>> (ct-edit-hct-t-dec seed 9))
+           ;; (b> (ct-edit-hct-t-dec seed 5))
+           ;; (b>> (ct-edit-hct-t-dec seed 9))
 
            (b> (ct-aedit-hct seed  (list h (* 1.5 c) (- tt 5))))
            (b>> (ct-aedit-hct seed (list h (* 2 c)   (- tt 8))))
 
            (b+ (-> b>
-                 (ct-edit-hct-c 100)
-                 (ct-complement-hct))))
+                 (ct-edit-hct-c 90)
+                 (ct-complement-hct)
+                 ;; (ct-aedit-hct-h (+ 180 60 h))
+                 )))
 
     (ht<-plist
       (list
         :focused (myron-mortal-colors b+)
-        :normal  (myron-mortal-colors b)
+        :normal  (myron-mortal-colors b t)
         :weak    (myron-mortal-colors b>)
         :strong  (myron-mortal-colors b>>)))))
 
 (deftheme myron-mortal)
 
 (myron-themes--define 'myron-mortal
-  ;; ehh
   ;; '((font-lock-comment-face :slant italic))
   `(
+     ;; todo: might not need to tweak org-link with type/highlight separation
+     (org-link :foreground
+
+       ;; ,(myron-themes-get :alt :focused)
+       ,(myron-themes-get :strings :weak)
+       )
      ;; (consult-preview-match :foreground "#ffffff")
      ((orderless-match-face-0 orderless-match-face-1 orderless-match-face-2 orderless-match-face-3)
-       :foreground ,(myron-themes-get :alt :focused))
-     )
-  )
+       :foreground
+       ,(->
+          (myron-themes-get :alt)
+          (ct-edit-hct-c 80)))))
 
 ;; (myron-themes-evil-cursor-color (myron-themes-get :assumed))
 
