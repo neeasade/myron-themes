06eba65e421bcf2c8834755df02307d25ae0a4e2
neeasade <neeasade@users.noreply.github.com>
2026-09-12 15:42:25 -0500

    refine

--- themes/myron-mortal-theme.el
+++ themes/myron-mortal-theme.el
@@ -6,62 +6,43 @@
 
 (defun myron-mortal-colors (background &optional print?)
   "Get the mortal foreground colors against a specific BACKGROUND."
-  (-let* (
-           (og-hue (ct-get-hct-h background))
-
+  (-let* ((og-hue (ct-get-hct-h background))
            ;; c is complement, a is original
-           ;; ｔｅｔｒａｄｉｃ
            ;; ((a b c d) (-iota 4 hue 90))
 
-           ;; to consider:
+           ;; ｔｅｔｒａｄｉｃ
            (o 60)
-           ;; pairs: ac and bd
            ((a b c d)
+             ;; pairs: ac and bd
              (list
                og-hue
                (+ og-hue o)
                (+ og-hue 180)
                (+ og-hue o 180)))
-           (contrast-boost .6)
-           ;; (contrast-boost 1)
-           )
+           (contrast-boost .6))
 
     (when print?
       ;; visualize
       (->> (list a b c d)
         (-map (lambda (hue)
-                (prn
+                (message
                   (-> background
                     (ct-edit-hct-t-dec 10)
                     (ct-edit-hct-h hue)
                     (ct-edit-hct-c 50)))))))
 
-
-    ;; todo: there's a problem here.
-    ;; we've been complecting "highlighting" and "types"
-    ;; maybe "types" can be "highlighting" with less chroma
-    ;; we need a meta -> highlight color for character matching, can't get away with it here
-
-
-    ;; okay what if we ignore the first hue. we h8 that shit anyway
     ;; "#ffc6b6"
     ;; "#dfd758"
     ;; "#57e4ff"
     ;; "#d3cdff"
-
     (->> (list
            ;; contrast hue chroma
            :foreground  4.0  a 7
            :assumed     4.0  c 40
-
-           :alt     3.5  b 20
-           :primary  3.0  a 100
-
-           :faded       2.5  a 40
+           :alt         3.5  b 20
+           :primary     3.0  a 100
+           :faded       2.5  b 25       ; this was initially 'a', wanted to spread abcd further
            :strings     3.0  d 50
-
-           ;; highlight value
-           ;; :alt         3    80  b
            )
       (-partition 4)
       (-mapcat (-lambda ((label contrast hue chroma))
@@ -78,25 +59,21 @@
   (-let* (
            ;; (ct-rotation-hct 12 "#fbe6e1")
            (seed "#fbe6e1")
-           (b seed)
 
-           (b (ct-edit-hct-t-inc seed 2))
+           ;; the seed origin. uncomment to randomize, but results vary (often bad)
+           ;; (seed (ns/random-list (ct-rotation-hct 12 "#fbe6e1")))
+
+           ;; (b seed)
+           ;; (b (ct-edit-hct-t-inc seed 2))
 
            (b (ct-edit-hct-t-inc seed 1.6))
-
-           ;; (b> (ct-edit-hct-t-dec seed 6))
-           ;; (b>> (ct-edit-hct-t-dec seed 12))
-
-           ;; (b> (ct-edit-hct-t-dec seed 5))
-           ;; (b>> (ct-edit-hct-t-dec seed 9))
-
            (b> (ct-aedit-hct seed  (list h (* 1.5 c) (- tt 5))))
            (b>> (ct-aedit-hct seed (list h (* 2 c)   (- tt 8))))
-
            (b+ (-> b>
                  (ct-complement-hct)
-                 (ct-edit-hct-c 25))))
-
+                 (ct-edit-hct-c 25)
+                 ;; this clamp allows tampering with b value for future me
+                 (ct-contrast-min b 1.1863))))
     (ht<-plist
       (list
         :focused (myron-mortal-colors b+)
@@ -109,18 +86,14 @@
 (myron-themes--define 'myron-mortal
   ;; '((font-lock-comment-face :slant italic))
   `(
-     ;; todo: might not need to tweak org-link with type/highlight separation
-     ;; (org-link :foreground
-     ;;   ;; ,(myron-themes-get :alt :focused)
-     ;;   ,(myron-themes-get :strings :weak)
-     ;;   )
-     ;; (consult-preview-match :foreground "#ffffff")
+     ;; todo
+     ;; we've been complecting "highlighting" and "types"
+     ;; maybe "types" can be "highlighting" with less chroma
+     ;; we need a meta -> highlight color for character matching, can't get away with it here
      ((orderless-match-face-0 orderless-match-face-1 orderless-match-face-2 orderless-match-face-3)
        :foreground
        ,(-> (myron-themes-get :strings)
           (ct-edit-hct-c 55)))))
-
-;; (myron-themes-evil-cursor-color (myron-themes-get :assumed))
 
 (provide-theme 'myron-mortal)
 
